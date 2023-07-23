11c9df761f382c8b2da05a4194b6f05d268c7b00
Welcome <jakdevops@gmail.com>
2023-07-23 00:44:21 -0700

    Roboshop scripts frontend

---
+++ catalogue.sh
@@ -0,0 +1,24 @@
+cp catalogue.service /etc/systemd/system/catalgoue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+systemctl daemon-reload
+
+yum install mongodb-org-shell -y
+mongo --host 172.31.89.179 </app/schema/catalogue.js
+
+systemctl enable catalogue
+systemctl restart catalogue
+
