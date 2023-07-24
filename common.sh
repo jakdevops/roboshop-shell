abe0b0c4290a5c7db6afadd244081c633d5198fe
Welcome <jakdevops@gmail.com>
2023-07-24 08:23:21 -0700

    functions for catalogue script

---
+++ common.sh
@@ -0,0 +1,50 @@
+nodejs () {
+  log=/tmp/roboshop.log
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> create user service file  >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  cp ${component}.service /etc/systemd/system/user.service &&>>${log}
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> create mongodb repo file >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> install nodejs repos >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> install nodejs >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  yum install nodejs -y &>>${log}
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> create application user >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  useradd roboshop &>>${log}
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> Remove application directory >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  rm -rf /app &>>${log}
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> create application directory >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  mkdir /app &>>${log}
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> download the application content >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> extract application content >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  cd /app
+  unzip /tmp/${component}.zip &>>${log}
+  cd /app
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> download nodejs dependencies >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  npm install &>>${log}
+
+
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> install mongodb client >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  yum install mongodb-org-shell -y &>>${log}
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> configure schema >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  mongo --host mongodb.jakdevops.online </app/schema/${component}.js &>>${log}
+
+  echo -e "\e[32m>>>>>>>>>>>>>>>>>>>>>>>>>>> start the user service  >>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a ${log}
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl restart ${component} &>>${log}
+
+
+}
